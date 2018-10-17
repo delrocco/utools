d4ca99e1fadebfaf9ca6b22f7149907792de72b5
Joe Del Rocco <delrocco@users.noreply.github.com>
2018-10-17 13:15:55 -0400

    Example of fitting a polynomial with numpy

---
+++ oneoffs/polyfit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ====================================================================
+# @author: Joe Del Rocco
+# @since: 10/17/2018
+# @summary: Script to fit poly to points and find inverse fit.
+# ====================================================================
+import numpy as np
+
+
+def main():
+    # fit polynomial to the specified points
+    x = np.array([0, 0.25, 0.5, 0.75, 1, 1.25, 1.5])
+    y = np.array([0, 0.17931912, 0.35597335, 0.5271127, 0.68979342, 0.840978, 0.97753519])
+    z = np.polyfit(x, y, 4)
+    print(z)
+
+    # find inverse fit
+    t = x
+    x = y
+    y = t
+    z = np.polyfit(x, y, 4)
+    print(z)
+
+if __name__ == "__main__":
+    main()
